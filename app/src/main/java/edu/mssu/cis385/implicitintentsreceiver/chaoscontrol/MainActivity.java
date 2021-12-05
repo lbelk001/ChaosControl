9268fb1344540d86ebc559913e19a5c9a88958ae
lbelk001 <belkl001@mymail.mssu.edu>
2021-12-04 22:46:44 -0600

    With dark mode

--- app/src/main/java/edu/mssu/cis385/implicitintentsreceiver/chaoscontrol/MainActivity.java
+++ app/src/main/java/edu/mssu/cis385/implicitintentsreceiver/chaoscontrol/MainActivity.java
@@ -1,15 +1,19 @@
 package edu.mssu.cis385.implicitintentsreceiver.chaoscontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -64,11 +68,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this,
                 PRIMARY_CHANNEL_ID)
                 .setContentTitle("Some Inspiration: ")
                 .setContentText("If the world did not suck, we would all fall off")
-                .setSmallIcon(R.drawable.ic_happy);
+                .setSmallIcon(R.drawable.ic_happy)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true);
         return notifyBuilder;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        }else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.night_mode){
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            recreate();
+        }
+        return true;
+    }
+
 }
